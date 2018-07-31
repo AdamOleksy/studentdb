fc6642cda8e25c0434c629211c78d68e670f9f33
Matijaz <matijaz@MacBook-Air-Matijaz.local>
2018-08-01 00:49:26 +0200

    Add valid PESEL generator

--- studentdb/3rd_stage/PeselValidator.hpp
+++ studentdb/3rd_stage/PeselValidator.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -9,17 +10,22 @@ class PeselValidator
     const int MIN_VALID_YEAR = 1900;
     const int MAX_VALID_YEAR = 2299;
     const int weights[10] = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3};
-    string PESEL;
-public:
-    PeselValidator(string pesel);
 
-    int getPeselDigit(int index);
-    bool isPeselValid();
-    int countCheckSum();
-    int getYear();
-    int getMonth();
-    int getDay();
+public:
+    PeselValidator();
+
+    int getPeselDigit(int index, string pesel);
+    bool isPeselValid(string pesel);
+    int countCheckSum(string pesel);
+    int getYear(string pesel);
+    int getMonth(string pesel);
+    int getDay(string pesel);
     bool isLeapYear(int year);
     bool isDateValid(int day, int month, int year);
+
+    void generateBirthDate(int &year, int &month, int &day);
+    int getMonthShiftedByYear(int year, int month);
+    int generateRandomNumber(int digitCount);
+    string generatePesel();
 };
 
